3f6c0afee8e7b1c253f6d33c84271260e33eed71
Rainer Borene <rainerborene@gmail.com>
2009-12-11 22:21:28 -0200

    New implementation.

--- extension.driver.php
+++ extension.driver.php
@@ -1,168 +1,156 @@
 <?php
 
 	require_once(TOOLKIT . '/class.entrymanager.php');
-	
-	
+	require_once(TOOLKIT . '/class.sectionmanager.php');
+
 	Class extension_static_section extends Extension{
-	
-		protected $section_data;
-		protected $_page;
-		protected $static_section_name;
-		
-		public function about(){
-			return array('name' => 'Static Section',
-						 'version' => '1.3',
-						 'release-date' => '2009-12-11',
-						 'author' => array('name' => 'Nathan Martin',
-										   'website' => 'http://knupska.com',
-										   'email' => 'nathan@knupska.com')
-				 		);
+
+		private $_sectionManager;
+		private $_entryManager;
+		private $_callback;
+		private $_static;
+
+		public function __construct($args){
+			$this->_Parent =& $args['parent'];
+
+			$this->_sectionManager = new SectionManager($this->_Parent);
+			$this->_entryManager = new EntryManager($this->_Parent);
+			$this->_callback = Administration::instance()->getPageCallback();
+			$this->_static = $this->isStaticSection();
 		}
-		
-		public function getSubscribedDelegates(){
+
+		public function about(){
 			return array(
-						array(
-							'page' => '/backend/',
-							'delegate' => 'InitaliseAdminPageHead',
-							'callback' => 'appendScriptToHead'
-						),
-						array(
-							'page' => '/administration/',
-							'delegate' => 'NavigationPreRender',
-							'callback' => 'applyStaticSections'
-						),
-						array(
-							'page' => '/administration/',
-							'delegate' => 'AdminPagePostGenerate',
-							'callback' => 'modifyPageElements'
-						)
+				'name' => 'Static Section',
+				'version' => '1.4',
+				'release-date' => '2009-12-11',
+				'author' => array(
+					'name' => 'Nathan Martin',
+					'website' => 'http://knupska.com',
+					'email' => 'nathan@knupska.com'
+				)
 			);
 		}
-		
-		public function appendScriptToHead($context) {
-			$this->static_section_name = '';
-			$entryManager = new EntryManager($this->_Parent);
-			$sections = $this->_Parent->Database->fetch("SELECT section_id AS id, handle FROM tbl_fields_static_section LEFT JOIN tbl_sections ON tbl_fields_static_section.section_id = tbl_sections.id");
-			$this->section_data = array(
-								'handles' => array(),
-								'entries' => array()
-								);
-			foreach($sections as $key => $value) {
-				$this->section_data['handles'][] = $value['handle'];
-				$result = $entryManager->fetch(NULL, $value['id'], NULL, NULL, NULL, NULL, false, false);
-				if (count($result) > 0) $this->section_data['entries'][] = $result[0]['id'];
-				else $this->section_data['entries'][] = NULL;
-			}
+
+		public function getSubscribedDelegates(){
+			return array(
+				array(
+					'page' => '/system/preferences/',
+					'delegate' => 'AddCustomPreferenceFieldsets',
+					'callback' => 'appendPreferences'
+				),
+				array(
+					'page' => '/backend/',
+					'delegate' => 'InitaliseAdminPageHead',
+					'callback' => 'applyStaticSection'
+				),
+				array(
+					'page' => '/administration/',
+					'delegate' => 'AdminPagePostGenerate',
+					'callback' => 'manipulateOutput'
+				)
+			);
+		}
+
+		public function appendPreferences($context){
+			$sections = Administration::instance()->Configuration->get('sections', 'static_section');
+
+			$group = new XMLElement('fieldset');
+			$group->setAttribute('class', 'settings');
+			$group->appendChild(new XMLElement('legend', 'Static Section'));
+
+			$label = Widget::Label();
+			$label->appendChild(Widget::Input('settings[static_section][sections]', $sections));
+			$group->appendChild($label);
+
+			$availableSections = $this->getAvailableSections();
+
+			$tags = new XMLElement('ul');
+			$tags->setAttribute('class', 'tags');
 			
-			$this->_page = Administration::instance()->Page;
-			$section_handle = $this->_page->_context['section_handle'];
-			$context = $this->_page->_context['page'];
-			$url_entry = $this->_page->_context['entry_id'];
-			$flag = $this->_page->_context['flag'];
-			
-			if (isset($section_handle)) {
-				$section = $this->_Parent->Database->fetchRow(0, "SELECT id, name FROM tbl_sections WHERE handle='$section_handle'");
-				$field = $this->_Parent->Database->fetchRow(0, "SELECT id FROM tbl_fields_static_section WHERE section_id=" . $section['id']);
-				
-				if($field) {
-					$entry = $this->getSectionEntry($section_handle);
-					
-					if ($context == 'new' && $entry) {
-						redirect(URL . '/symphony/publish/' . $section_handle . '/edit/' . $entry . '/');
-					}
-					
-					if($context == 'index') {
-						if ($entry) redirect(URL . '/symphony/publish/' . $section_handle . '/edit/' . $entry . '/');
-						else redirect(URL . '/symphony/publish/' . $section_handle . '/new/');
-					}
-					
-					if ($context == 'edit') {
-						if(!$entry) redirect(URL . '/symphony/publish/' . $section_handle . '/new/');
-						if($url_entry != $entry) redirect(URL . '/symphony/publish/' . $section_handle . '/edit/' . $entry . '/');
-					}
-					
-					if ($flag == 'saved' || $flag == 'created') {
-						$flag_msg = 'Entry updated at %1$s.';
-						if ($flag == 'created') $flag_msg = 'Entry created at %1$s.';
-						$this->_page->pageAlert(
-							__(
-								$flag_msg, 
-								array(DateTimeObj::getTimeAgo(__SYM_TIME_FORMAT__))
-							), 
-							Alert::SUCCESS);
-					}
-					
-					$this->static_section_name = $section['name'];
+			foreach($availableSections as $section){
+				$tags->appendChild(new XMLElement('li', $section));
+			}
+
+			$group->appendChild($tags);
+
+			$context['wrapper']->appendChild($group);
+		}
+
+		public function applyStaticSection($context){
+			if ($this->_callback['driver'] == 'publish' && $this->_static){
+				$section_handle = $this->_callback['context']['section_handle'];
+				$entry = $this->getLastPosition($section_handle);
+
+				if ($this->_callback['context']['entry_id'] != $entry || $this->_callback['context']['page'] == 'index'){
+					redirect(URL . "/symphony/publish/{$section_handle}/edit/{$entry}/");
+				}
+
+				if (!$entry && $this->_callback['context']['page'] != 'new'){
+					redirect(URL . "/symphony/publish/{$section_handle}/new/");
 				}
 			}
 		}
-		
-		private function getSectionEntry($handle) {
-			$array_location = $this->getSectionEntryPosition($handle);
-			if ($array_location !== FALSE) return $this->section_data['entries'][$array_location];
-			return NULL;
-		}
-		
-		private function getSectionEntryPosition($handle) {
-			return array_search($handle, $this->section_data['handles']);
-		}
-		
-		public function applyStaticSections($nav) {
-			foreach( $nav['navigation'] as $pkey => $pvalue) {
-				if ($pkey < 100) {
-					foreach($pvalue['children'] as $key => $value) {
-						if ($value['visible'] == 'yes') {
-							$is_static = $this->getSectionEntryPosition($value['section']['handle']);
-							if($is_static !== FALSE) {
-								$entry = $this->getSectionEntry($value['section']['handle']);
-								if ($entry) $nav['navigation'][$pkey]['children'][$key]['link'] = '/publish/' . $value['section']['handle'] . '/edit/' . $entry . '/';
-								else $nav['navigation'][$pkey]['children'][$key]['link'] = '/publish/' . $value['section']['handle'] . '/new/';
-							}
-						}
-					}
+
+		public function manipulateOutput($context){
+			if ($this->_static){
+				$section_id = $this->_sectionManager->fetchIDFromHandle($this->_callback['context']['section_handle']);
+				$section = !is_null($section_id) ? $this->_sectionManager->fetch($section_id) : null;
+
+				$dom = DOMDocument::loadHTML($context['output']);
+				$xpath = new DOMXPath($dom);
+
+				$title = $xpath->query("/html/head/title")->item(0);
+				$title->nodeValue = __('%1$s &ndash; %2$s', array(__('Symphony'), $section->get('name')));
+
+				if ($this->_callback['context']['page'] == 'edit'){
+					$delete = $xpath->query("//div[@class='actions']/button[@name='action[delete]']")->item(0);
+					$delete->parentNode->removeChild($delete);
 				}
+
+				$context['output'] = $dom->saveHTML();
 			}
 		}
-		
-		public function modifyPageElements($output) {
-			if ($this->static_section_name != '') {
-				// force static section title
-				$startpos = strpos($output['output'], '<h2>');
-				$endpos = strpos($output['output'], '</h2>', $startpos);
-				if ($startpos !== FALSE && $endpos !== FALSE) {
-					$output['output'] = substr_replace($output['output'], '<h2>' . $this->static_section_name . '</h2>', $startpos, $endpos - $startpos);
-				}
-				
-				// hide delete button
-				$startpos = strpos($output['output'], '<button name="action[delete]"');
-				$endpos = strpos($output['output'], '</button>', $startpos);
-				if ($startpos !== FALSE && $endpos !== FALSE) {
-					$output['output'] = substr_replace($output['output'], '', $startpos, $endpos - $startpos);
-				}
+
+		private function isStaticSection(){
+			$sections = explode(',', Administration::instance()->Configuration->get('sections', 'static_section'));
+
+			foreach($sections as $i => $s)
+				$sections[$i] = trim($s);
+
+			if (is_array($this->_callback['context']) && in_array($this->_callback['context']['section_handle'], $sections)) return true;
+
+			return false;
+		}
+
+		private function getLastPosition($section_handle){
+			$this->_entryManager->setFetchSortingDirection('DESC');
+
+			$section_id = $this->_sectionManager->fetchIDFromHandle($section_handle);
+			$entry = $this->_entryManager->fetch(NULL, $section_id, 1);
+
+			if (is_array($entry) && !empty($entry)){
+				$entry = end($entry);
+				return $entry->get('id');
 			}
 		}
-		
+
+		private function getAvailableSections(){
+			$sections = $this->_sectionManager->fetch();
+			$result = array();
+
+			if (is_array($sections) && !empty($sections)){
+				foreach($sections as $s){
+					$result[] = $s->get('handle');
+				}
+			}
+
+			return $result;
+		}
+
 		public function uninstall(){
-			$this->_Parent->Database->query("DROP TABLE `tbl_fields_static_section`");
+			return Administration::instance()->Configuration->remove('sections', 'static_section');
 		}
-		
-		public function update($previousVersion) {
-			// for future use
-			return true;
-		}
-		
-		public function install() {
-			return $this->_Parent->Database->query("CREATE TABLE `tbl_fields_static_section` (
-			  `id` int(11) unsigned NOT NULL auto_increment,
-			  `field_id` int(11) unsigned NOT NULL,
-			  `section_id` int(11) unsigned NOT NULL,
-			  PRIMARY KEY  (`id`),
-			  UNIQUE KEY `field_id` (`field_id`),
-			  UNIQUE KEY `section_id` (`section_id`)
-			) TYPE=MyISAM");
-			return true;
-		}
-		
 	}
 
 ?>
