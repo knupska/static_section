48537376883c0a29bce51dd8371b000a59763947
Marcio Toledo <mt@marciotoledo.com>
2011-02-24 13:46:39 +0800

    Added Portuguese (Brazil) translation

---
+++ lang/lang.pt-br.php
@@ -0,0 +1,27 @@
+<?php
+
+	$about = array(
+		'name' => 'Portuguese (Brazil)',
+		'author' => array(
+			'name' => 'Marcio Toledo',
+			'email' => 'mt@marciotoledo.com',
+			'website' => 'http://marciotoledo.com'
+		),
+		'release-date' => '2011-02-24'
+	);
+	
+	
+	
+	/*
+	 * EXTENSION: Static Sections
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+
+		'Make this section static (i.e. a single entry section)' =>
+		'Seção estática (exemplo, uma seção de entrada única)'
+
+	);
+
+?>
